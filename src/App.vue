0d739a4b43b0e86561b31f76c5af369619d1d230
ngandung <gandungn@gmail.com>
2024-12-09 10:12:29 +0700

    vue animation

---
+++ src/App.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="container">
+    <div class="block" :class="{animate: animatedBlock}"></div>
+    <button @click="animateBlock">Animate</button>
+  </div>
+  <div class="container">
+    
+    <!-- <Transition> is Vue default component to control animation -->
+    <!-- Transition only habve one direct element -->
+     <transition>
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
+  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+    <p>This is a test dialog!</p>
+    <button @click="hideDialog">Close it!</button>
+  </base-modal>
+  <div class="container">
+    <button @click="showDialog">Show Dialog</button>
+  </div>
+
+  <!-- <div class="container">
+      <Transition>
+        <p v-if="show">hello</p>
+      </Transition>
+      <button @click="show = !show">Toggle</button>
+  </div> -->
+</template>  
+
+<script>
+export default {
+  data() {
+    return { 
+      dialogIsVisible: false,
+      animatedBlock: false,
+      paraIsVisible: false,
+      show: false,
+     };
+  },
+  methods: {
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
+    },
+    animateBlock() {
+      this.animatedBlock = true;
+    },
+    showDialog() {
+      this.dialogIsVisible = true;
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+button {
+  font: inherit;
+  padding: 0.5rem 2rem;
+  border: 1px solid #810032;
+  border-radius: 30px;
+  background-color: #810032;
+  color: white;
+  cursor: pointer;
+}
+button:hover,
+button:active {
+  background-color: #a80b48;
+  border-color: #a80b48;
+}
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #290033;
+  margin-bottom: 2rem;
+  /* transition: transform 0.3s ease-out; */
+}
+.container {
+  max-width: 40rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 12px;
+}
+
+.animate {
+  /* transform: translateX(-150px); */
+  /* forwards is use so animation stay in final state and not backward */
+  animation: slide-fade 1s ease-out forwards;
+}
+
+
+
+@keyframes slide-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
+}
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active {
+  transition: all 1s ease-in;
+}
+
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0px);
+}
+
+/* we will explain what these classes do next! */
+/* .v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+} */
+</style>
